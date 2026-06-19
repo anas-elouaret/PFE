b95efbb9d8ac8466ba691d6c3b3ab022122bb3cc
anas-elouaret <elouaretanas480@gmail.com>
2026-06-19 16:26:39 +0200

    f

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,19 @@
-const serverless = require("serverless-http");
-const app = require("../app");
+let app;
+try {
+  app = require("../app");
+} catch (err) {
+  console.error("Require error:", err);
+  module.exports = (req, res) => {
+    res.status(500).json({ error: err.message, stack: err.stack });
+  };
+  return;
+}
 
-module.exports = serverless(app);
+module.exports = async (req, res) => {
+  try {
+    await app(req, res);
+  } catch (err) {
+    console.error("Handler error:", err);
+    res.status(500).json({ error: err.message, stack: err.stack });
+  }
+};
